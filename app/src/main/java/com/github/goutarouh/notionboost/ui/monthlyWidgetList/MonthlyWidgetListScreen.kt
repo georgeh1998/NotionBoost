88557ec6b6e2075476f190760cb6a7b6197b0aff
goutarouh <gouta.gorogoro@gmail.com>
2024-01-06 07:28:21 +0900

    Add Empty screen.

--- app/src/main/java/com/github/goutarouh/notionboost/ui/monthlyWidgetList/MonthlyWidgetListScreen.kt
+++ app/src/main/java/com/github/goutarouh/notionboost/ui/monthlyWidgetList/MonthlyWidgetListScreen.kt
@@ -1,14 +1,19 @@
 package com.github.goutarouh.notionboost.ui.monthlyWidgetList
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -16,17 +21,52 @@ fun MonthlyWidgetListScreen(
     uiModel: MonthlyWidgetListUiModel,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(
+    Box(
         modifier = modifier.fillMaxSize(),
     ) {
-        items(uiModel.widgets) {
-            MonthlyWidgetItem(
-                widget = it,
+        if (uiModel.widgets.isEmpty()) {
+            MonthlyWidgetListEmpty(
                 modifier = Modifier,
             )
+        } else {
+            LazyColumn(
+                modifier = modifier.fillMaxSize(),
+            ) {
+                items(uiModel.widgets) {
+                    MonthlyWidgetItem(
+                        widget = it,
+                        modifier = Modifier,
+                    )
+                }
+            }
         }
     }
+}
 
+@Composable
+private fun MonthlyWidgetListEmpty(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Monthly widgets list",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(vertical = 12.dp)
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = "This screen shows a list of your added app widgets.\n" +
+                    "Let's enhance your device's home screen \n by adding 'Habit Tracker (Monthly)' widgets.",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(vertical = 12.dp),
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Composable
